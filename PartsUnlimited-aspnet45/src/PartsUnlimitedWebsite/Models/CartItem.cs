81adb755cdba2248556c402825c9896ffcf5ad98
Luis Garza <lj.garza@outlook.com>
2020-09-17 18:25:40 -0500

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //Test change
+        //updated change
         [Key]
         public int CartItemId { get; set; }
 
